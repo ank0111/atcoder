200a480e8b8f37a2faeb837f88996faf0919134a
ank0111 <la.sose.thky@gmail.com>
2022-09-17 14:11:20 +0900

    feature: add graph library

---
+++ my-library/graph.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Graph
+{
+    Graph(int n=0) : _n(n), _g(n), _dis(n, INT_MAX), _prev(n, -1) {}
+    void resize(int n)
+    {
+        _n = n;
+        _g.resize(n);
+    }
+    void load(int m, bool d = false)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            u--, v--;
+            assert(0 <= u && u < _n);
+            assert(0 <= v && v < _n);
+            _g[u].push_back(v);
+            if (!d)
+                _g[v].push_back(u);
+        }
+    }
+    void load(vector<pair<int, int>> e, bool d = false)
+    {
+        for (auto [u, v] : e)
+        {
+            assert(0 <= u && u < _n);
+            assert(0 <= v && v < _n);
+            _g[u].push_back(v);
+            if (!d)
+                _g[v].push_back(u);
+        }
+    }
+    int size()
+    {
+        return _n;
+    }
+    vector<vector<int>> graph()
+    {
+        return _g;
+    }
+    vector<int> dis(int s = 0)
+    {
+        assert(0 <= s && s < _n);
+        queue<int> q;
+        _dis[s] = 0;
+        q.push(s);
+        while (q.size())
+        {
+            int t = q.front();
+            q.pop();
+            for (int n : _g[t])
+            {
+                if (_dis[n] > _dis[t] + 1)
+                {
+                    _dis[n] = _dis[t] + 1;
+                    _prev[n] = t;
+                    q.push(n);
+                }
+            }
+        }
+        return _dis;
+    }
+    vector<int> prev()
+    {
+        return _prev;
+    }
+    tuple<int, int, int> tdia()
+    {
+        dis();
+        int s = max_element(_dis.begin(), _dis.end()) - _dis.begin();
+        dis(s);
+        int t = max_element(_dis.begin(), _dis.end()) - _dis.begin();
+        return {_dis[t], s, t};
+    }
+
+private:
+    int _n;
+    vector<vector<int>> _g;
+    vector<int> _dis, _prev;
+};
