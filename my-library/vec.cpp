8422d06bb8d93d14d55cd799bc7c50770e278119
ank0111 <la.sose.thky@gmail.com>
2022-09-23 16:06:40 +0900

    add vec

---
+++ my-library/vec.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace vec
+{
+    template <typename T>
+    T vsum(const vector<T> &v)
+    {
+        T sum = 0;
+        for (const T &x : v)
+            sum += x;
+        return sum;
+    }
+    template <typename T>
+    void distinct(vector<T> &v)
+    {
+        sort(v.begin(), v.end());
+        v.erase(unique(v.begin(), v.end()), v.end());
+    }
+    template <typename T>
+    T vmax(const vector<T> &v) { return *max_element(v.begin(), v.end()); }
+    template <typename T>
+    T vmin(const vector<T> &v) { return *min_element(v.begin(), v.end()); }
+    template <typename T>
+    vector<T> comp(vector<T> &a)
+    {
+        vector<T> ret;
+        set<T> s;
+        for (const T &x : a)
+            s.insert(x);
+        map<T, int> idx;
+        int i = 0;
+        for (const T &x : s)
+        {
+            idx[x] = i;
+            ret.pb(x);
+            i++;
+        }
+        for (T &x : a)
+            x = idx[x];
+        return ret;
+    }
+}
+
+using vi = vector<int>;
+using namespace vec;
+
+int main()
+{
+    vi v{0, 2, 4, 5, 2};
+}
