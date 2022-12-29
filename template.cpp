2afec268ff78023b30d8b5f495d73344e341e128
ank0111 <la.sose.thky@gmail.com>
2022-12-30 00:24:59 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -513,7 +513,6 @@ namespace my_lib
     using vt = std::vector<T>;
     using vvt = std::vector<vt>;
     Mat() {}
-    Mat(size_t r, T id = 1) : vvt(r), _r(r), _id(id) {}
     Mat(size_t r, vt v, T id = 1) : vvt(r, v), _r(r), _c(v.size()), _id(id) {}
     Mat(vvt m, T id = 1) : vvt(m), _r(m.size()), _c(m[0].size()), _id(id) {}
     Mat(size_t r, size_t c, T id = 1) : vvt(r, vt(c)), _r(r), _c(c), _id(id) {}
@@ -549,6 +548,76 @@ namespace my_lib
         }
         x *= x;
         n >>= 1;
+      }
+      return res;
+    }
+    Mat base()
+    {
+      Mat res = *this;
+      size_t rank = 0;
+      const double EPS = 1e-10;
+      for (size_t j = 0; j < _c; j++)
+      {
+
+        int pivot = -1;
+        T ma = EPS;
+        for (size_t i = rank; i < _r; i++)
+        {
+          if (abs(res[i][j]) > ma)
+          {
+            ma = abs(res[i][j]);
+            pivot = i;
+          }
+        }
+        if (pivot == -1)
+          continue;
+        std::swap(res[pivot], res[rank]);
+        auto fac = res[rank][j];
+        for (size_t k = 0; k < _c; k++)
+          res[rank][k] /= fac;
+        for (size_t i = 0; i < _r; i++)
+        {
+          if (i != rank && abs(res[i][j]) > EPS)
+          {
+            auto fac = res[i][j];
+            for (size_t k = 0; k < _c; k++)
+            {
+              res[i][k] -= res[rank][k] * fac;
+            }
+          }
+        }
+        rank++;
+      }
+      return res;
+    }
+    Mat xorbase()
+    {
+      Mat res = *this;
+      int rank = 0;
+      for (int j = 0; j < _c; j++)
+      {
+        int pivot = -1;
+        for (int i = rank; i < _r; i++)
+        {
+          if (res[i][j])
+          {
+            pivot = i;
+          }
+        }
+        if (pivot == -1)
+          continue;
+        std::swap(res[pivot], res[rank]);
+        for (int i = 0; i < _r; i++)
+        {
+          if (i != rank && res[i][j])
+          {
+            for (int k = 0; k < _c; k++)
+            {
+              res[i][k] ^= res[rank][k];
+            }
+          }
+        }
+        rank++;
       }
       return res;
     }
