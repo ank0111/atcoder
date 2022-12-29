2089220703480c5de2e359ec6409e01c9546b6bf
ank0111 <la.sose.thky@gmail.com>
2022-12-30 00:24:44 +0900

    Update mat.cpp

--- my-library/mat.cpp
+++ my-library/mat.cpp
@@ -9,7 +9,6 @@ namespace my_lib
         using vt = std::vector<T>;
         using vvt = std::vector<vt>;
         Mat() {}
-        Mat(size_t r, T id = 1) : vvt(r), _r(r), _id(id) {}
         Mat(size_t r, vt v, T id = 1) : vvt(r, v), _r(r), _c(v.size()), _id(id) {}
         Mat(vvt m, T id = 1) : vvt(m), _r(m.size()), _c(m[0].size()), _id(id) {}
         Mat(size_t r, size_t c, T id = 1) : vvt(r, vt(c)), _r(r), _c(c), _id(id) {}
@@ -45,6 +44,75 @@ namespace my_lib
                 }
                 x *= x;
                 n >>= 1;
+            }
+            return res;
+        }
+        Mat base()
+        {
+            Mat res = *this;
+            int rank = 0;
+            const double EPS = 1e-10;
+            for (int j = 0; j < _c; j++)
+            {
+                int pivot = -1;
+                T ma = EPS;
+                for (int i = rank; i < _r; i++)
+                {
+                    if (abs(res[i][j]) > ma)
+                    {
+                        ma = abs(A[i][j]);
+                        pivot = i;
+                    }
+                }
+                if (pivot == -1)
+                    continue;
+                std::swap(res[pivot], res[rank]);
+                auto fac = res[rank][j];
+                for (int k = 0; k < _c; k++)
+                    res[rank][k] /= fac;
+                for (int i = 0; i < _r; i++)
+                {
+                    if (i != rank && abs(res[i][j]) > EPS)
+                    {
+                        auto fac = res[i][j];
+                        for (int k = 0; k < _c; k++)
+                        {
+                            res[i][k] -= res[rank][k] * fac;
+                        }
+                    }
+                }
+                rank++;
+            }
+            return res;
+        }
+        Mat xorbase()
+        {
+            Mat res = *this;
+            int rank = 0;
+            for (int j = 0; j < _c; j++)
+            {
+                int pivot = -1;
+                for (int i = rank; i < _r; i++)
+                {
+                    if (res[i][j])
+                    {
+                        pivot = i;
+                    }
+                }
+                if (pivot == -1)
+                    continue;
+                std::swap(res[pivot], res[rank]);
+                for (int i = 0; i < _r; i++)
+                {
+                    if (i != rank && res[i][j])
+                    {
+                        for (int k = 0; k < _c; k++)
+                        {
+                            res[i][k] ^= res[rank][k];
+                        }
+                    }
+                }
+                rank++;
             }
             return res;
         }
