64c37dd1d6704c2b6d840222f7957d6768b3d364
ank0111 <la.sose.thky@gmail.com>
2023-04-04 18:01:55 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -955,6 +955,53 @@ namespace my_lib
       return res;
     }
   };
+  struct Mo
+  {
+    Mo(int n = 0) : n(n){};
+    void add(int l, int r)
+    {
+      lr.emplace_back(l, r);
+      if (r > n)
+        n = r;
+    }
+    template <typename AL, typename AR, typename EL, typename ER, typename O>
+    void solve(AL &al, AR &ar, EL &el, ER &er, O &out)
+    {
+      int q = (int)lr.size();
+      int bs = n / std::min<int>(n, sqrt(q));
+      std::vector<int> ord(q);
+      std::iota(ord.begin(), ord.end(), 0);
+      std::sort(ord.begin(), ord.end(), [&](int l, int r)
+                { int lb = lr[l].first / bs, rb = lr[r].first / bs;
+                      if (lb!=rb)return lb<rb;
+                      if(lb&1)return lr[l].second>lr[r].second;
+                      return lr[l].second<lr[r].second; });
+      int l = 0, r = 0;
+      for (int idx : ord)
+      {
+        auto [ql, qr] = lr[idx];
+        while (l > ql)
+          al(--l);
+        while (r < qr)
+          ar(r++);
+        while (l < ql)
+          el(l++);
+        while (r > qr)
+          er(--r);
+        out(idx);
+      }
+    }
+
+    template <typename A, typename E, typename O>
+    void solve(A &add, E &erase, O &out)
+    {
+      solve(add, add, erase, erase, out);
+    }
+
+  private:
+    int n;
+    std::vector<std::pair<int, int>> lr;
+  };
   ll sqsum(ll a, ll b) { return a * a + b * b; }
   ll sq(ll a) { return a * a; }
   ll fact(ll n)
